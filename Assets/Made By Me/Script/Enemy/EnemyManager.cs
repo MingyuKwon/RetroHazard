a7f8e55a98f94fbac64db3824be4c735583db723
Kwon Mingyu <kmgmg230601@gmail.com>
2023-01-28 22:26:10 +0900

    change parry color and effect

--- Assets/Made By Me/Script/Enemy/EnemyManager.cs
+++ Assets/Made By Me/Script/Enemy/EnemyManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public bool isEnemyPaused = false;
+    public bool isParried = false;
     EnemyStatus status;
 
     private void Awake() {
         status = GetComponentInChildren<EnemyStatus>();
+    }
+
+    public void SetEnemyParried()
+    {
+        
     }
 
 }
