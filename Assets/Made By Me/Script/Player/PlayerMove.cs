a304424b9cf6acbacdba72aa4722161951e68a4b
Kwon Mingyu <kmgmg230601@gmail.com>
2023-02-25 17:30:30 +0900

    make Player moving bug(while entering up, down input CANNOT be retreived)

--- Assets/Made By Me/Script/Player/PlayerMove.cs
+++ Assets/Made By Me/Script/Player/PlayerMove.cs
@@ -9,9 +9,17 @@ public class PlayerMove : MonoBehaviour
 {
     private Player player;
 
+
     [Header("changeable stat")]
     public float moveSpeed = 1f;
     public bool canMove = true;
+
+    public bool InputOk;
+
+    public bool UpInputBlock;
+    public bool DownInputBlock;
+    public bool RightInputBlock;
+    public bool LeftInputBlock;
 
 
     [Header("Debug stat")]
@@ -21,33 +29,41 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] float HorizontalMoveSpeed;
     [SerializeField] float VerticalMoveSpeed;
 
+    float HorizontalMoveSpeedFinal;
+    float VerticalMoveSpeedFinal;
+
     private void Awake() {
         player = ReInput.players.GetPlayer(0);
+
+        player.AddInputEventDelegate(UPPressed, UpdateLoopType.Update, InputActionEventType.ButtonPressed, "Move Up");
+        player.AddInputEventDelegate(DownPressed, UpdateLoopType.Update, InputActionEventType.ButtonPressed, "Move Down");
+        player.AddInputEventDelegate(RightPressed, UpdateLoopType.Update, InputActionEventType.ButtonPressed,"Move Right");
+        player.AddInputEventDelegate(LeftPressed, UpdateLoopType.Update, InputActionEventType.ButtonPressed,"Move Left");
+
+        player.AddInputEventDelegate(UPJustPressed, UpdateLoopType.Update, InputActionEventType.ButtonJustPressed, "Move Up");
+        player.AddInputEventDelegate(DownJustPressed, UpdateLoopType.Update, InputActionEventType.ButtonJustPressed, "Move Down");
+        player.AddInputEventDelegate(RightJustPressed, UpdateLoopType.Update, InputActionEventType.ButtonJustPressed,"Move Right");
+        player.AddInputEventDelegate(LeftJustPressed, UpdateLoopType.Update, InputActionEventType.ButtonJustPressed,"Move Left");
+
+        player.AddInputEventDelegate(UPJustReleased, UpdateLoopType.Update, InputActionEventType.ButtonJustReleased, "Move Up");
+        player.AddInputEventDelegate(DownJustReleased, UpdateLoopType.Update, InputActionEventType.ButtonJustReleased, "Move Down");
+        player.AddInputEventDelegate(RightJustReleased, UpdateLoopType.Update, InputActionEventType.ButtonJustReleased,"Move Right");
+        player.AddInputEventDelegate(LeftJustReleased, UpdateLoopType.Update, InputActionEventType.ButtonJustReleased,"Move Left");
     }
     private void FixedUpdate() {
 
         if(canMove && !(GameManager.instance.isPlayerPaused))
         {
-            if(player.GetButton("Move Up"))
+            InputOk = true;
+
+            if(!player.GetButton("Move Right") && !player.GetButton("Move Left"))
             {
-                VerticalMoveSpeed =  1f;
-            }else if(player.GetButton("Move Down"))
-            {
-                VerticalMoveSpeed =  -1f;
-            }else
-            {
-                VerticalMoveSpeed =  0f;
+                HorizontalMoveSpeed = 0f;
             }
 
-            if(player.GetButton("Move Right"))
+            if(!player.GetButton("Move Up") && !player.GetButton("Move Down"))
             {
-                HorizontalMoveSpeed =  1f;
-            }else if(player.GetButton("Move Left"))
-            {
-                HorizontalMoveSpeed =  -1f;
-            }else
-            {
-                HorizontalMoveSpeed =  0f;
+                VerticalMoveSpeed = 0f;
             }
 
             if(VerticalMoveSpeed != 0f && HorizontalMoveSpeed != 0f)
@@ -58,11 +74,105 @@ public class PlayerMove : MonoBehaviour
                 moveForceSchloar = 1f;
             }
             
-            VerticalMoveSpeed =  VerticalMoveSpeed * moveSpeed * Time.fixedDeltaTime * moveForceSchloar;
-            HorizontalMoveSpeed = HorizontalMoveSpeed * moveSpeed * Time.fixedDeltaTime * moveForceSchloar;
+            VerticalMoveSpeedFinal =  VerticalMoveSpeed * moveSpeed * Time.fixedDeltaTime * moveForceSchloar;
+            HorizontalMoveSpeedFinal = HorizontalMoveSpeed * moveSpeed * Time.fixedDeltaTime * moveForceSchloar;
 
-            transform.position = new Vector2( transform.position.x + HorizontalMoveSpeed , transform.position.y + VerticalMoveSpeed);
+            transform.position = new Vector2( transform.position.x + HorizontalMoveSpeedFinal , transform.position.y + VerticalMoveSpeedFinal);
+        }else
+        {
+            InputOk = false;
         }
     }
+
+        // keep presseing
+    void UPPressed(InputActionEventData data)
+    {
+        if(!InputOk ) return;
+        if(UpInputBlock) return;
+        VerticalMoveSpeed = 1f;
+    }
+
+    void DownPressed(InputActionEventData data)
+    {
+        if(!InputOk ) return;
+        if(DownInputBlock) return;
+        VerticalMoveSpeed = -1f;
+    }
+
+    void RightPressed(InputActionEventData data)
+    {
+        if(!InputOk ) return;
+        if(RightInputBlock) return;
+        HorizontalMoveSpeed =  1f;
+    }
+
+    void LeftPressed(InputActionEventData data)
+    {
+        if(!InputOk ) return;
+        if(LeftInputBlock) return;
+        HorizontalMoveSpeed =  -1f;
+    }
+    // keep presseing
+
+    void UPJustPressed(InputActionEventData data)
+    {
+        if(player.GetButton("Move Down"))
+        {
+            DownInputBlock = true;
+        }
+        UpInputBlock = false;
+    }
+
+    void DownJustPressed(InputActionEventData data)
+    {
+        if(player.GetButton("Move Up"))
+        {
+            UpInputBlock = true;
+        } 
+        DownInputBlock = false;
+    }
+
+    void RightJustPressed(InputActionEventData data)
+    {
+        if(player.GetButton("Move Left"))
+        {
+            LeftInputBlock = true;
+        } 
+        RightInputBlock = false;
+    }
+
+    void LeftJustPressed(InputActionEventData data)
+    {
+        if(player.GetButton("Move Right"))
+        {
+            RightInputBlock = true;
+        } 
+        LeftInputBlock = false;
+    }
+
+
+    // just the time release the button
+    void UPJustReleased(InputActionEventData data)
+    {
+        DownInputBlock = false;
+    }
+
+    void DownJustReleased(InputActionEventData data)
+    {        
+        UpInputBlock = false;
+    }
+
+    void RightJustReleased(InputActionEventData data)
+    {        
+        LeftInputBlock = false;
+    }
+
+    void LeftJustReleased(InputActionEventData data)
+    {        
+        RightInputBlock = false;
+    }
+    // just the time release the button
+
+    
 
 }
