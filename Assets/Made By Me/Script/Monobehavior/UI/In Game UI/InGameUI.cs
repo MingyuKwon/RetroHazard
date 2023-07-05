0af3023069953d32457c66af96b0ae01447fdbe0
Kwon Mingyu <kmgmg230601@gmail.com>
2023-07-06 00:47:56 +0900

    포켓몬 처럼 내려오는거 만들었다

--- Assets/Made By Me/Script/Monobehavior/UI/In Game UI/InGameUI.cs
+++ Assets/Made By Me/Script/Monobehavior/UI/In Game UI/InGameUI.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
 
 /////// Cleared ////////
 public class InGameUI : MonoBehaviour
@@ -7,10 +9,33 @@ public class InGameUI : MonoBehaviour
     PlayerHealthUI playerHealthUI;
     SheildDurabilityUI sheildDurabilityUI;
     EnergyUI energyUI;
+    MapShowPanel mapShowPanel;
+
+    float showingTime = 1f;
+
+    Vector2 invisiblePosition = new Vector2(200, 70);
+    Vector2 fullVisiblePosition = new Vector2(200, -10);
+
+    private RectTransform mapShowPanelRecTransform;
+    private Text mapShowPanelText;
+
+    bool canStartCoroutine = true;
+
+    ////////// 보간을 역행하기 위한 배열 /////////////////
+    float[] saveYPosition;
+    ////////// 보간을 역행하기 위한 배열 /////////////////
+
     private void Awake() {
         playerHealthUI = GetComponentInChildren<PlayerHealthUI>();
         sheildDurabilityUI = GetComponentInChildren<SheildDurabilityUI>();
         energyUI = GetComponentInChildren<EnergyUI>();
+        mapShowPanel = GetComponentInChildren<MapShowPanel>();
+
+        mapShowPanelRecTransform = mapShowPanel.gameObject.GetComponent<RectTransform>();
+        mapShowPanelText = mapShowPanel.gameObject.GetComponentInChildren<Text>();
+
+        mapShowPanelRecTransform.anchoredPosition = invisiblePosition;
+
     }
 
     private void OnEnable() {
@@ -29,6 +54,68 @@ public class InGameUI : MonoBehaviour
             StartCoroutine(UpdateUIDelay());
         }
         
+    }
+
+    [Button]
+    public void ShowMapShowPanel(string mapName)
+    {
+        if(!canStartCoroutine) return;
+
+        canStartCoroutine = false;
+        mapShowPanelRecTransform.anchoredPosition = invisiblePosition;
+        mapShowPanelText.text = mapName;
+        StartCoroutine(ShowMapShowPanelCoroutine());
+    }
+
+    public IEnumerator ShowMapShowPanelCoroutine()
+    {
+        float fullShowTimeScholar = 0.3f;
+
+        float showingTimeHalf = (showingTime * (1 - fullShowTimeScholar) / 2) / Time.unscaledDeltaTime;
+        float fullShowingTime = showingTime * fullShowTimeScholar / Time.unscaledDeltaTime;
+
+        saveYPosition = new float[Mathf.FloorToInt(showingTimeHalf) + 2];
+        
+        saveYPosition[0] = mapShowPanelRecTransform.anchoredPosition.y;
+
+        for(int i=0; i<Mathf.FloorToInt(showingTimeHalf)+1; i++)
+        {
+            MoveShowMapShowPanel(0.1f, i, true);
+            yield return new WaitForEndOfFrame();
+        }
+
+        for(int i=0; i<Mathf.FloorToInt(fullShowingTime); i++)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        for(int i=0; i<Mathf.FloorToInt(showingTimeHalf)+2; i++)
+        {
+            MoveShowMapShowPanel(0.1f, i, false);
+            yield return new WaitForEndOfFrame();
+        }
+
+        saveYPosition = null;
+        canStartCoroutine = true;
+    }
+
+    private void MoveShowMapShowPanel(float C ,int index , bool isShowing) // index 안주면 되돌리기
+    {
+        if(isShowing)
+        {
+            float LerpedYValue =  Mathf.Lerp(mapShowPanelRecTransform.anchoredPosition.y, fullVisiblePosition.y, C);
+            saveYPosition[index+1] = LerpedYValue;
+            mapShowPanelRecTransform.anchoredPosition = new Vector2(mapShowPanelRecTransform.anchoredPosition.x, LerpedYValue);
+        
+        }else
+        {
+            int reverseIndex = saveYPosition.Length - index - 1;
+
+            mapShowPanelRecTransform.anchoredPosition = 
+                new Vector2(mapShowPanelRecTransform.anchoredPosition.x, saveYPosition[reverseIndex]);
+        }
+
+       
     }
 
     public IEnumerator UpdateUIDelay()
