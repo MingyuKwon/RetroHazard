237c9220534e6447268cac704ba1f0069adb326d
Kwon Mingyu <kmgmg230601@gmail.com>
2023-07-05 23:09:58 +0900

    콜라이더 위를 지나다닐 때 위치의 이름을 알려주는 collider 만듬

---
+++ Assets/Made By Me/Script/Monobehavior/Interactive/MapShowInteract.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapShowInteract : MonoBehaviour
+{
+    public MapNameCollection.sceneName sceneName;
+
+    [Header("is Passage is Vertiacl? or Horizontal")]
+    public bool isVertical;
+
+    [Header("What is the name of Plus side of Colldier? what about Minus?")]
+    public int inPlus;
+    public int inMinus;
+
+    Vector2 enterVector;
+
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player Body") )
+        {
+            enterVector = Player1.instance.playerMove.playerDirection;
+            
+            if(isVertical)
+            {
+                float angle = Vector2.Angle(Vector2.up, enterVector);
+                if (angle < 90f)
+                {
+                    enterVector = Vector2.up;
+                }else
+                {
+                    enterVector = Vector2.down;
+                }
+            }else
+            {
+                float angle = Vector2.Angle(Vector2.right, enterVector);
+                if (angle < 90f)
+                {
+                    enterVector = Vector2.right;
+                }else
+                {
+                    enterVector = Vector2.left;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player Body") )
+        {
+            float angle = Vector2.Angle(Player1.instance.playerMove.playerDirection, enterVector);
+
+            if (angle < 90f) // 유턴 안하고 직진으로 빠져나감 -> 바꿔야 함
+            {
+                if(isVertical)
+                {
+                    if(enterVector == Vector2.up)
+                    {
+                        Debug.Log(MapNameCollection.getMapNameArray((int)sceneName)[inPlus]);
+                    }else if(enterVector == Vector2.down)
+                    {
+                        Debug.Log(MapNameCollection.getMapNameArray((int)sceneName)[inMinus]);
+                    }
+                }else
+                {
+                    if(enterVector == Vector2.right)
+                    {
+                        Debug.Log(MapNameCollection.getMapNameArray((int)sceneName)[inPlus]);
+                    }else if(enterVector == Vector2.left)
+                    {
+                        Debug.Log(MapNameCollection.getMapNameArray((int)sceneName)[inMinus]);
+                    }
+                }
+
+            
+            }else // 유턴 하고 있던 곳으로 되돌아감 -> 표시 안함
+            {
+                
+            }
+        }
+    }
+}
---
+++ Assets/Made By Me/Script/Monobehavior/MapNameCollection.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapNameCollection 
+{
+    public enum sceneName{
+        Tutorial = 0,
+    }
+
+    public static string[] getMapNameArray(int num)
+    {
+        switch(num)
+        {
+            case 0:
+                return tutorialMapName;
+        }
+
+        Debug.Log("getMapNameArray NULL");
+        return null;
+    }
+
+    public static string[] tutorialMapName = {
+        "Adeenhau beach",
+        "Noshuc archaeological site",
+    };
+}
